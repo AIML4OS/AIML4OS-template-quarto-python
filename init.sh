c836ad07e7657f883e51b043e9a9f68dc7e208a8
Olivier Meslin <44379737+oliviermeslin@users.noreply.github.com>
2025-08-26 16:56:51 +0200

    Use vanilla uv

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@
 # Get the name of the repo
 MY_REPO=$(ls -d "/home/onyxia/work"/*/ | head -n 1 | xargs basename)
 
-# Install all dependencies in the system folder
-uv pip install -r $MY_REPO/pyproject.toml --system
+# Create a virtual environment and install project dependencies
+uv sync -r $MY_REPO/pyproject.toml
 
 # Download data
 sh $MY_REPO/download_data.sh
