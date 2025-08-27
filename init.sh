829176b5646ebd0fd2ace3df16c86f7b73eb223b
oliviermeslin <olivier.meslin@insee.fr>
2025-08-27 08:57:00 +0000

    Modify the main init script

--- init.sh
+++ init.sh
@@ -3,21 +3,8 @@
 # Get the name of the repo
 MY_REPO=$(ls -d "/home/onyxia/work"/*/ | head -n 1 | xargs basename)
 
-# Change directory
-cd $MY_REPO
-
-# Create a virtual environment and install project dependencies
-uv sync
-
-# Change directory
-cd ..
-
-# Set VSCode's default interpreter path
-mkdir -p .vscode
-echo "{ \"python.defaultInterpreterPath\": \"./$MY_REPO/.venv/bin/python\" }" > .vscode/settings.json
-
-# Set Quarto default interpreter path
-export QUARTO_PYTHON=./$MY_REPO/.venv/bin/python
+# Restore the environment
+sh $MY_REPO/sspcloud/restore_environment.sh
 
 # Download data
-sh $MY_REPO/download_data.sh
+sh $MY_REPO/sspcloud/download_data.sh
