57cddd8ec41ee963b54fb1cfa9b9ef98e86d3e29
oliviermeslin <olivier.meslin@insee.fr>
2025-08-27 08:31:39 +0000

    Improving the init script

--- init.sh
+++ init.sh
@@ -3,8 +3,21 @@
 # Get the name of the repo
 MY_REPO=$(ls -d "/home/onyxia/work"/*/ | head -n 1 | xargs basename)
 
+# Change directory
+cd $MY_REPO
+
 # Create a virtual environment and install project dependencies
-uv sync -r $MY_REPO/pyproject.toml
+uv sync
+
+# Change directory
+cd ..
+
+# Set VSCode's default interpreter path
+mkdir -p .vscode
+echo "{ \"python.defaultInterpreterPath\": \"./$MY_REPO/.venv/bin/python\" }" > .vscode/settings.json
+
+# Set Quarto default interpreter path
+export QUARTO_PYTHON=./$MY_REPO/.venv/bin/python
 
 # Download data
 sh $MY_REPO/download_data.sh
