62f9fe38867df2d4ab16c2961c912540f87d1584
oliviermeslin <olivier.meslin@insee.fr>
2025-08-27 08:56:46 +0000

    Add an init script

---
+++ sspcloud/restore_environment.sh
@@ -0,0 +1,15 @@
+# Change directory
+cd $MY_REPO
+
+# Create a virtual environment and install project dependencies
+uv sync
+
+# Change directory
+cd ..
+
+# Set VSCode's default interpreter path
+mkdir -p .vscode
+echo "{ \"python.defaultInterpreterPath\": \"./$MY_REPO/.venv/bin/python\" }" >> .vscode/settings.json
+
+# Set Quarto default interpreter path
+export QUARTO_PYTHON=./$MY_REPO/.venv/bin/python
